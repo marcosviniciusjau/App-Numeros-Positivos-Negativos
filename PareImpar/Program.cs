cd180fbe675e20f34a7300ef9279e680ae6b0131
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2022-12-16 18:55:58 -0300

    Primeiro Commit

---
+++ PareImpar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PareImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+
+
+            } catch (Exception ex)
+            {
+
+            }
+            double numero_qualquer;
+
+            Console.WriteLine("Digite um número qualquer");
+            numero_qualquer = Convert.ToDouble(Console.ReadLine());
+
+            if (numero_qualquer > 0)
+        }
+            Console.WriteLine("Você digitou um número positivo");
+
+        }  else if(numero_qualquer < 0)
+            Console.WriteLine("Você digitou um número negativo");
+
+        } else /*if(numero_qualquer ==0)*/
+          Console.WriteLine("Você digitou zero.");
+        }
+    }
+}
